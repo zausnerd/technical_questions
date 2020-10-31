1971d0af88dc22e2fd6de275ae1ef20ff52c3177
David Zausner <davidzausner@Davids-MBP.home>
2020-10-30 23:25:48 -0400

    kth subarray

---
+++ kth_subarray.py
@@ -0,0 +1,46 @@
+# Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
+#
+# The list is very long, so making more than one pass is prohibitively expensive.
+#
+# Do this in constant space and in one pass.
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            cursor = self.head
+            while cursor.next is not None:
+                cursor = cursor.next
+            cursor.next = Node(value)
+    def print_values(self):
+        cursor = self.head
+        while cursor is not None:
+            print(cursor.value)
+            cursor = cursor.next
+
+def kth_last_element(node, k):
+    kth_node = node
+    cursor_node = node
+    for	i in range(0, k + 1):
+        kth_node = kth_node.next
+    while kth_node is not None:
+        kth_node = kth_node.next
+        cursor_node = cursor_node.next
+    print(cursor_node.value)
+ll = LinkedList()
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+ll.insert(4)
+ll.insert(5)
+ll.insert(6)
+ll.insert(7)
+ll.insert(8)
+kth_last_element(ll.head, 2)
